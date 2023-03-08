51948cb4339315d3c422441695a44fa1f92806c9
Junbum Lee <jun@beomi.net>
2023-03-08 14:38:34 +0900

    Support args, and kwargs @v0.1.3

--- gptpy/gptpy.py
+++ gptpy/gptpy.py
@@ -7,13 +7,15 @@ import traceback
 import requests
 
 
-def run_file(file_path, *args):
+def run_file(file_path, args_list, kwargs_dict):
     # execute the specified file
     try:
         f = open(file_path, "r")
         txt = f.read()
         # exec(txt)
-        subprocess.check_call(["python", file_path] + list(args))
+        cmd_args = ["python", file_path] + args_list
+        cmd_kwargs = {f"--{k}": str(v) for k, v in kwargs_dict.items()}
+        subprocess.check_call(cmd_args, **cmd_kwargs)
     except Exception as e:
         exception_log = traceback.format_exc()
         r = requests.post(
@@ -30,7 +32,8 @@ def run_file(file_path, *args):
         # you can also log or handle the exception here
 
 
-def main():  # define command-line arguments
+def main():
+    # define command-line arguments
     parser = argparse.ArgumentParser(
         description="Run a Python file with a custom wrapper"
     )
@@ -42,8 +45,18 @@ def main():  # define command-line arguments
     # parse the arguments
     args = parser.parse_args()
 
+    # extract the args and kwargs from the arguments
+    args_list = []
+    kwargs_dict = {}
+    for arg in args.args:
+        if arg.startswith("--"):
+            key, val = arg[2:].split("=", maxsplit=1)
+            kwargs_dict[key] = val
+        else:
+            args_list.append(arg)
+
     # run the file with the wrapper
-    run_file(args.file, *args.args)
+    run_file(args.file, args_list, kwargs_dict)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gptpy",
-    version="0.1.2",
+    version="0.1.3",
     description="GPTPy: Your kind Python guide, powered by AI to fix errors and explain code",
     author="Junbum Lee",
     author_email="junbumlee@icloud.com",
